ef1667c9ba6086a431b92b1c605f5c34401f58aa
elnas <elnasiru@gmail.com>
2024-09-16 16:44:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@
 function loadFooter() {
     const footerContent = `
         <div class="social-media">
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="#">
+          <!--     <a href="https://www.instagram.com" target="_blank">  -->
                 <img src="images/instagram2.png" alt="Instagram" class="social-icon">
             </a>
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="#">
+         <!--      <a href="https://www.facebook.com" target="_blank">  -->
                 <img src="images/facebook3.png" alt="Facebook" class="social-icon">
             </a>
-            <a href="https://www.twitter.com" target="_blank">
+            <a href="#">
+            <!--   <a href="https://www.twitter.com" target="_blank">  -->
                 <img src="images/x1.png" alt="Twitter" class="social-icon">
             </a>
         </div>
-        <p>&copy; <a href="index.html">2024 Open-Seminar/Workshop Sassari, Sardinia</a> |  All rights reserved | Powered by <a href="https://www.iaas.uni-stuttgart.de/abteilung-service-computing/" target="_blank">IAAS-SC</a></p>
+        <p>&copy; <a href="index.html">2024 Workshop on Service-Oriented Computing and Current Trends, Sassari, Sardinia</a> |  All rights reserved | Powered by <a href="https://www.iaas.uni-stuttgart.de/abteilung-service-computing/" target="_blank">IAAS-SC</a></p>
     `;
 
     // Get all footer elements on the page
